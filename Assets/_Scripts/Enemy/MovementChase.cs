9fa61e23a7a392332ffba3094ae13a82b17b5808
LiteFeather1 <102820899+LiteFeather1@users.noreply.github.com>
2023-10-25 18:10:33 +0100

    Enemies Sprite

--- Assets/_Scripts/Enemy/MovementChase.cs
+++ Assets/_Scripts/Enemy/MovementChase.cs
@@ -3,13 +3,18 @@
 public class MovementChase : Movement
 {
     [SerializeField] private Transform _target;
+    [SerializeField] private bool _followX = false;
+    [SerializeField] private float _xDirection = -1f;
 
     public void SetTarget(Transform target) => _target = target;
 
     private void Update()
     {
+        Vector2 target = _target.position;
+        if (!_followX)
+            target.x = transform.position.x + _xDirection;
         transform.position = Vector2.MoveTowards(transform.position,
-                                                 _target.position,
+                                                 target,
                                                  _speed * Time.deltaTime);
     }
 }
