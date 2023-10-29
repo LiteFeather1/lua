e0a56531e2270ff13e39ea51654a52139ecd7131
LiteFeather1 <102820899+LiteFeather1@users.noreply.github.com>
2023-10-29 12:11:37 +0000

    Moon Sprite Chaging

---
+++ Assets/_Scripts/MoonSprite.cs
@@ -0,0 +1,14 @@
+﻿using MoonPhaseConsole;
+using UnityEngine;
+
+public class MoonSprite : MonoBehaviour
+{
+    [SerializeField] private SpriteRenderer _sr;
+    [SerializeField] private Sprite[] _moonPhases;
+
+    public void Start()
+    {
+        var result = Moon.UtcNow();
+        _sr.sprite = _moonPhases[result.Index];
+    }
+}
