37fc753a6a9e6ea87fe0a5d25262d9c4762f8ab5
LiteFeather1 <102820899+LiteFeather1@users.noreply.github.com>
2023-11-14 15:49:22 +0000

    Added knockback card

--- Assets/_Scripts/Power_Ups/Witch/PowerUpKnockback.cs
+++ Assets/_Scripts/Power_Ups/Witch/PowerUpKnockback.cs
@@ -1,6 +1,6 @@
 ﻿using UnityEngine;
 
-[CreateAssetMenu(menuName = "Power Up/Witch/Knockback")]
+[CreateAssetMenu(menuName = "Power Up/Witch/Knockback 1")]
 public class PowerUpKnockback : PowerUpModifier
 {
     public override void ApplyEffect(GameManager gm)
--- Assets/_Scripts/State_Machine/State_Movements/MovementKnockback.cs
+++ Assets/_Scripts/State_Machine/State_Movements/MovementKnockback.cs
@@ -5,10 +5,10 @@ public class MovementKnockback : StateMachine.State
     private const int TIMES_TO_SHAKE = 7;
 
     [Header("Knockback State")]
-    private float _duration = .20f;
-    private float _speed = .75f;
-    private float _knockbackForce = 1f;
+    [SerializeField] private float _duration = .15f;
+    [SerializeField] private float _speed = .5f;
     [SerializeField] private Vector2 _direction;
+    private float _knockbackForce = 1f;
     private readonly Vector2 _rockyOffsetRange = new(.2f, .25f);
     private Vector2 _offset;
     private int _timesShaked;
@@ -26,7 +26,7 @@ public class MovementKnockback : StateMachine.State
         {
             _timesShaked++;
             float sign = _timesShaked % 2 == 0 ? 1f : -1f;
-            _offset = new(_rockyOffsetRange.Random() / 2f, _rockyOffsetRange.Random() * sign);
+            _offset.Set(_rockyOffsetRange.Random() / 2f, _rockyOffsetRange.Random() * sign);
         }
 
         var delta = Time.deltaTime * _speed * _knockbackForce;
