949b9d537c47605f60ec9dafe99e6f284b9645e5
LiteFeather1 <102820899+LiteFeather1@users.noreply.github.com>
2023-11-16 12:35:07 +0000

    Optional Value

---
+++ Assets/_Scripts/Attributes/OptionalPropertyDrawer.cs
@@ -0,0 +1,40 @@
+using UnityEngine;
+using UnityEditor;
+using LTFUtils;
+
+#if UNITY_EDITOR
+[CustomPropertyDrawer(typeof(OptionalValue<>))]
+public class OptionalPropertyDrawer : PropertyDrawer
+{
+    private const float TOGGLE_PAD = 24;
+
+    public override float GetPropertyHeight(SerializedProperty property, GUIContent label)
+    {
+        SerializedProperty valueProperty = property.FindPropertyRelative("_value");
+        return EditorGUI.GetPropertyHeight(valueProperty, label);
+    }
+
+    public override void OnGUI(Rect position, SerializedProperty property, GUIContent label)
+    {
+        SerializedProperty valueProperty = property.FindPropertyRelative("_value");
+        SerializedProperty enabledProperty = property.FindPropertyRelative("_enabled");
+
+        EditorGUI.BeginProperty(position, label, property);
+
+        position.width -= TOGGLE_PAD;
+        EditorGUI.BeginDisabledGroup(!enabledProperty.boolValue);
+        EditorGUI.PropertyField(position, valueProperty, label, true);
+        EditorGUI.EndDisabledGroup();
+
+        int indent = EditorGUI.indentLevel;
+        EditorGUI.indentLevel = 0;
+        position.x += TOGGLE_PAD + position.width;
+        position.width = position.height = EditorGUI.GetPropertyHeight(enabledProperty);
+        position.x -= position.width;
+        EditorGUI.PropertyField(position, enabledProperty, GUIContent.none);
+        EditorGUI.indentLevel = indent;
+
+        EditorGUI.EndProperty();
+    }
+}
+#endif
---
+++ Assets/_Scripts/Helpers/OptionalValue.cs
@@ -0,0 +1,75 @@
+using System;
+using UnityEngine;
+
+namespace LTFUtils
+{
+    [Serializable]
+    public struct OptionalValue<T> : IEquatable<OptionalValue<T>>
+    {
+        [SerializeField] private T _value;
+        [SerializeField] private bool _enabled;
+
+        public readonly bool Enabled => _enabled;
+        public readonly T Value => _value;
+
+        public OptionalValue(T initialValue)
+        {
+            _value = initialValue;
+            _enabled = true;
+        }
+
+        public OptionalValue(T inivialValue, bool enabled)
+        {
+            _value = inivialValue;
+            _enabled = enabled;
+        }
+
+        public OptionalValue(bool enabled)
+        {
+            _value = default;
+            _enabled = enabled;
+        }
+
+        public readonly T GetValue() => _enabled ? _value : default;
+
+        #region Operators
+        public static implicit operator OptionalValue<T>(T v) => new(v);
+
+        public static implicit operator T(OptionalValue<T> o) => o._value;
+
+        public static implicit operator bool(OptionalValue<T> o) => o.Enabled;
+
+        public static bool operator ==(OptionalValue<T> left, OptionalValue<T> right)
+        {
+            if (left._value is null)
+            {
+                if (right._value is null)
+                {
+                    // null == null = true.
+                    return true;
+                }
+
+                // Only the left side is null.
+                return false;
+            }
+            // Equals handles the case of null on right side.
+            return left._value.Equals(right._value);
+        }
+
+        public static bool operator !=(OptionalValue<T> lhs, OptionalValue<T> rhs) => !(lhs == rhs);
+
+        public override readonly bool Equals(object obj)
+        {
+            // return base.Equals(obj);
+            return _value.Equals(obj);
+        }
+
+        public readonly bool Equals(OptionalValue<T> other) => _value.Equals(other);
+
+        public override readonly int GetHashCode() => _value.GetHashCode();
+
+        public override readonly string ToString() => _value.ToString();
+
+        #endregion
+    }
+}
