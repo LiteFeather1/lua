a94cd952a8bfc04479bf75e0262c4f7b842fb67a
LiteFeather1 <102820899+LiteFeather1@users.noreply.github.com>
2023-11-17 15:43:19 +0000

    Experimenting with a property drawer to only draw in playmode or in editor

---
+++ Assets/_Scripts/Attributes/DrawInEditorModeAttribute.cs
@@ -0,0 +1,33 @@
+﻿using UnityEditor;
+using UnityEngine;
+
+public class DrawInEditorModeAttribute : PropertyAttribute
+{
+    public bool InEditor { get; }
+
+    public DrawInEditorModeAttribute(bool inEditor = true) => InEditor = inEditor;
+}
+
+#if UNITY_EDITOR
+[CustomPropertyDrawer(typeof(DrawInEditorModeAttribute))]
+public class DrawInPlayModeDrawer : PropertyDrawer
+{
+    private float _propertyDrawer;
+
+    public override float GetPropertyHeight(SerializedProperty property, GUIContent label)
+    {
+        return _propertyDrawer;
+    }
+
+    public override void OnGUI(Rect position, SerializedProperty property, GUIContent label)
+    {
+        if ((attribute as DrawInEditorModeAttribute).InEditor != Application.isPlaying)
+        {
+            _propertyDrawer = base.GetPropertyHeight(property, label);
+            EditorGUI.PropertyField(position, property, label, true);
+        }
+        else
+            _propertyDrawer = 0f;
+    }
+}
+#endif
