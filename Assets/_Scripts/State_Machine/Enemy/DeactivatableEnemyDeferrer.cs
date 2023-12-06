57481b77083fcc452f9106bfee43038f531b1016
LiteFeather1 <102820899+LiteFeather1@users.noreply.github.com>
2023-12-06 21:25:34 +0000

    Fixed a bug where ghost was not being deactivated

---
+++ Assets/_Scripts/State_Machine/Enemy/DeactivatableEnemyDeferrer.cs
@@ -0,0 +1,8 @@
+﻿using UnityEngine;
+
+public class DeactivatableEnemyDeferrer : MonoBehaviour, IDeactivatable
+{
+    [SerializeField] private Enemy _enemy;
+
+    public void Deactivate() => _enemy.Deactivate();
+}
