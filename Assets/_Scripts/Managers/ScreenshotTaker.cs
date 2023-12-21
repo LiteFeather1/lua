8961cfdc92f082a1d25ba70f98e1056a3f8bd1af
LiteFeather1 <102820899+LiteFeather1@users.noreply.github.com>
2023-12-21 20:07:57 +0000

    Screenshot taker to facilitate taking pictures

---
+++ Assets/_Scripts/Managers/ScreenshotTaker.cs
@@ -0,0 +1,22 @@
+﻿using UnityEngine;
+
+#if UNITY_EDITOR
+public class ScreenshotTaker : MonoBehaviour
+{
+    private int _session;
+    private int _run;
+    private int _screenShotsTaken;
+
+    private void Awake()
+    {
+        _session = PlayerPrefsHelper.GetSessions();
+        _run = PlayerPrefsHelper.GetRuns();
+    }
+
+    private void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.F2))
+            ScreenCapture.CaptureScreenshot($"Assets/Screenshots/Session_{_session}_Run_{_run}_{_screenShotsTaken++}.png");
+    }
+}
+#endif
